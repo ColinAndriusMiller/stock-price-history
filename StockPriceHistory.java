3e90294e7012e7511d4dc913b340a38eef0955ef
Colin Miller <colin.miller@mrjaffesclass.com>
2023-02-28 11:41:38 -0800

    ok?

--- StockPriceHistory.java
+++ StockPriceHistory.java
@@ -1,18 +1,36 @@
-/**
- * Retrieve historical stock prices
- */
+import yahoofinance.YahooFinance;
+import yahoofinance.histquotes.Interval;
+import yahoofinance.histquotes.HistoricalQuote;
+import yahoofinance.*;
+import java.util.Calendar;
+import java.util.List; 
+import java.util.Map;
+import java.util.HashMap;
+import java.io.File;
+import java.io.FileWriter;
+
 public class StockPriceHistory
 {
 
     private final String TICKER = "GOOG";
+    private Interval DAILY = Interval.DAILY;
     
     /**
      * Retrieve the stock price data
      */
     public void run() {
         try {
+            Calendar from = Calendar.getInstance();
             Stock stock = YahooFinance.get(TICKER, true);
+            Calendar to = Calendar.getInstance();
             System.out.println(stock);
+            from.add(Calendar.YEAR, -1);
+            System.out.println(stock);
+            
+            Stock google = YahooFinance.get("GOOG");
+            List<HistoricalQuote> googleHistQutoes = google.getHistory(to, Interval.DAILY);
+            Map<String, String> datas = new HashMap<String, String>();
+            
         } catch (Exception e) {
             System.out.println("Error in stock call");    
         }
